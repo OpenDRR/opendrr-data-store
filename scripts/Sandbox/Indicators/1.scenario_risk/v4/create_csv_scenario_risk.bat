8c18fd76098a963e81fb33c152803e8c0e47487d
wkhchow <59062243+wkhchow@users.noreply.github.com>
2020-05-08 16:21:56 -0700

    psql sandbox development

---
+++ scripts/Sandbox/Indicators/1.scenario_risk/v4/create_csv_scenario_risk.bat
@@ -0,0 +1,23 @@
+@echo OFF
+@SET PGPASSFILE=C:\Users\William Chow\AppData\Roaming\postresql\pgpass.conf
+
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\1.create_scenario_risk_rlz_0_b0_building.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\1.create_scenario_risk_rlz_0_r1_building.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\1.create_scenario_risk_rlz_0_r2_building.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\1.create_scenario_risk_rlz_1_b0_building.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\1.create_scenario_risk_rlz_1_r1_building.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\1.create_scenario_risk_rlz_1_r2_building.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\1.create_scenario_risk_rlz_2_b0_building.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\1.create_scenario_risk_rlz_2_r1_building.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\1.create_scenario_risk_rlz_2_r2_building.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\2.create_scenario_risk_rlz_0_b0_sauid.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\2.create_scenario_risk_rlz_0_r1_sauid.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\2.create_scenario_risk_rlz_0_r2_sauid.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\2.create_scenario_risk_rlz_1_b0_sauid.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\2.create_scenario_risk_rlz_1_r1_sauid.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\2.create_scenario_risk_rlz_1_r2_sauid.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\2.create_scenario_risk_rlz_2_b0_sauid.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\2.create_scenario_risk_rlz_2_r1_sauid.psql"
+"C:\Program Files\PostgreSQL\12\bin\psql.exe" -U postgres -d oq_sandbox -f "D:\workspace\GitHub\opendrr-data-store\scripts\Sandbox\Indicators\1.scenario_risk\v4\export csv\2.create_scenario_risk_rlz_2_r2_sauid.psql"
+
+pause
