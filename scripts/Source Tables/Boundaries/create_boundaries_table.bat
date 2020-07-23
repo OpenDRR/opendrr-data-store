f1c7279f884a1ba4d7014ed2d0ed80b44bb121b8
wkhchow <59062243+wkhchow@users.noreply.github.com>
2020-07-23 14:41:27 -0700

    updated geometry table SAUID
    Updated to reflect new name boundaries."Geometry_SAUID"

--- scripts/Source Tables/Boundaries/create_boundaries_table.bat
+++ scripts/Source Tables/Boundaries/create_boundaries_table.bat
@@ -1,12 +1,20 @@
 @echo OFF
 
 REM create boundaries schema geometry tables from default geopackages.  Change ogr2ogr.exe PATH / geopackage path if nessessary to run.
-"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\data\human-settlement-national\layers\Geometry ADAUID.gpkg" -nln boundaries."Geometry ADAUID" -lco LAUNDER=NO 
-"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\data\human-settlement-national\layers\Geometry CANADA.gpkg" -nln boundaries."Geometry CANADA" -lco LAUNDER=NO 
-"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\data\human-settlement-national\layers\Geometry CDUID.gpkg" -nln boundaries."Geometry CDUID" -lco LAUNDER=NO 
-"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\data\human-settlement-national\layers\Geometry CSDUID.gpkg" -nln boundaries."Geometry CSDUID" -lco LAUNDER=NO 
-"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\data\human-settlement-national\layers\Geometry ERUID.gpkg" -nln boundaries."Geometry ERUID" -lco LAUNDER=NO 
-"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\data\human-settlement-national\layers\Geometry FSAUID.gpkg" -nln boundaries."Geometry FSAUID" -lco LAUNDER=NO 
-"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\data\human-settlement-national\layers\Geometry SAUID.gpkg" -nln boundaries."Geometry SAUID" -lco LAUNDER=NO 
+
+"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\GitHub\boundaries\Geometry_ADAUID.gpkg" -nln boundaries."Geometry_ADAUID" -lco LAUNDER=NO 
+
+"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\GitHub\boundaries\Geometry_CANADA.gpkg" -nln boundaries."Geometry_CANADA" -lco LAUNDER=NO 
+
+"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\GitHub\boundaries\Geometry_CDUID.gpkg" -nln boundaries."Geometry_CDUID" -lco LAUNDER=NO 
+
+"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\GitHub\boundaries\Geometry_CSDUID.gpkg" -nln boundaries."Geometry_CSDUID" -lco LAUNDER=NO 
+
+"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\GitHub\boundaries\Geometry_ERUID.gpkg" -nln boundaries."Geometry_ERUID" -lco LAUNDER=NO
+ 
+"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\GitHub\boundaries\Geometry_FSAUID.gpkg" -nln boundaries."Geometry_FSAUID" -lco LAUNDER=NO 
+
+"C:\OSGeo4W64\bin\ogr2ogr.exe" -f "PostgreSQL" PG:"host=localhost user=postgres dbname=oq_sandbox password=password" "D:\workspace\GitHub\boundaries\Geometry_SAUID.gpkg" -nln boundaries."Geometry_SAUID" -lco LAUNDER=NO 
 
 pause
+
