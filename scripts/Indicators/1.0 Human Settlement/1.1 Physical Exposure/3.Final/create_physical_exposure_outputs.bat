243884940f0d4a643c7cb109c378b032f14ccbf3
wkhchow <59062243+wkhchow@users.noreply.github.com>
2020-08-27 16:42:10 -0700

    updates

--- scripts/Indicators/1.0 Human Settlement/1.1 Physical Exposure/3.Final/create_physical_exposure_outputs.bat
+++ scripts/Indicators/1.0 Human Settlement/1.1 Physical Exposure/3.Final/create_physical_exposure_outputs.bat
@@ -2,11 +2,11 @@ REM Export physical exposure views to geojson, and geopackage files. Replace exp
 
 REM Geojson export
 FOR %%x IN (nhsl_physical_exposure_buildings_b,nhsl_physical_exposure_people_b,physical_exposure_assets_b,nhsl_physical_exposure_settled_area_s,nhsl_physical_exposure_building_function_s,nhsl_physical_exposure_building_type_s,
-nhsl_physical_exposure_people_s,nhsl_physical_exposure_assets_s,) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "GeoJSON" D:\workspace\data\view_outputs\%%x.geojson PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_physical_exposure.%%x
+nhsl_physical_exposure_people_s,nhsl_physical_exposure_assets_s) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "GeoJSON" D:\workspace\data\view_outputs\%%x.geojson PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_physical_exposure.%%x
 
 REM Geopackage Shapefile export
 FOR %%x IN (nhsl_physical_exposure_buildings_b,nhsl_physical_exposure_people_b,physical_exposure_assets_b,nhsl_physical_exposure_settled_area_s,nhsl_physical_exposure_building_function_s,nhsl_physical_exposure_building_type_s,
-nhsl_physical_exposure_people_s,nhsl_physical_exposure_assets_s,) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_physical_exposure.%%x
+nhsl_physical_exposure_people_s,nhsl_physical_exposure_assets_s) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_physical_exposure.%%x
 
 PAUSE
 
