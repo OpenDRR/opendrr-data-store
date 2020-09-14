9aafc4c8e72bf463102ca564f225d8227b55d8cb
wkhchow <59062243+wkhchow@users.noreply.github.com>
2020-09-14 16:57:23 -0700

    export updates

---
+++ scripts/Data Export/create_nhsl_all_indicator_outputs.bat
@@ -0,0 +1,17 @@
+REM Export views to geojson, and geopackage files. Replace exposure name {bldgexp_canada} to target exposure if different to run.  Change ogr2ogr path, location paths, db information if needed.
+
+
+REM Geopackage Shapefile export
+FOR %%x IN (nhsl_physical_exposure_all_b) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_physical_exposure.%%x
+
+REM Geopackage Shapefile export
+FOR %%x IN (nhsl_risk_dynamics_all_indicators_s) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_risk_dynamics.%%x
+
+REM Geopackage Shapefile export
+FOR %%x IN (nhsl_social_fabric_all_indicators_s) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_social_fabric.%%x
+
+REM Geopackage Shapefile export
+FOR %%x IN (nhsl_hazard_threat_all_indicators_s) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_hazard_threat.%%x
+
+
+PAUSE
--- scripts/Data Export/create_risk_dynamics_outputs.bat
+++ scripts/Data Export/create_risk_dynamics_outputs.bat
@@ -1,9 +1,9 @@
 REM Export views to geojson, and geopackage files. Replace exposure name {bldgexp_canada} to target exposure if different to run.  Change ogr2ogr path, location paths, db information if needed.
 
 REM Geojson export
-FOR %%x IN (nhsl_risk_dynamics_hazard_susceptibility_s_tbl,nhsl_risk_dynamics_land_use_change_s_tbl,nhsl_risk_dynamics_population_growth_s_tbl) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "GeoJSON" D:\workspace\data\view_outputs\%%x.geojson PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_risk_dynamics.%%x
+FOR %%x IN (nhsl_risk_dynamics_hazard_susceptibility_s,nhsl_risk_dynamics_land_use_change_s,nhsl_risk_dynamics_population_growth_s) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "GeoJSON" D:\workspace\data\view_outputs\%%x.geojson PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_risk_dynamics.%%x
 
 REM Geopackage Shapefile export
-FOR %%x IN (nhsl_risk_dynamics_hazard_susceptibility_s_tbl,nhsl_risk_dynamics_land_use_change_s_tbl,nhsl_risk_dynamics_population_growth_s_tbl) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_risk_dynamics.%%x
+FOR %%x IN (nhsl_risk_dynamics_hazard_susceptibility_s,nhsl_risk_dynamics_land_use_change_s,nhsl_risk_dynamics_population_growth_s) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_risk_dynamics.%%x
 
 PAUSE
