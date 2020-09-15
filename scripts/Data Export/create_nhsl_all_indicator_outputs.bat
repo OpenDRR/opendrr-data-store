f8748028541273d98f92c01c07826d07cc500bb6
wkhchow <59062243+wkhchow@users.noreply.github.com>
2020-09-14 17:10:18 -0700

    update export

--- scripts/Data Export/create_nhsl_all_indicator_outputs.bat
+++ scripts/Data Export/create_nhsl_all_indicator_outputs.bat
@@ -2,7 +2,7 @@ REM Export views to geojson, and geopackage files. Replace exposure name {bldgex
 
 
 REM Geopackage Shapefile export
-FOR %%x IN (nhsl_physical_exposure_all_b) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_physical_exposure.%%x
+FOR %%x IN (nhsl_physical_exposure_all_indicators_b) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_physical_exposure.%%x
 
 REM Geopackage Shapefile export
 FOR %%x IN (nhsl_risk_dynamics_all_indicators_s) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_risk_dynamics.%%x
