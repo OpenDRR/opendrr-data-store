f195f0c273e08f1f50aaf00aa7c83686d586a6e0
wkhchow <59062243+wkhchow@users.noreply.github.com>
2020-10-07 13:14:50 -0700

    updates

--- scripts/Data Export/create_nhsl_all_indicator_outputs.bat
+++ scripts/Data Export/create_nhsl_all_indicator_outputs.bat
@@ -1,5 +1,7 @@
 REM Export views to geojson, and geopackage files. Replace exposure name {bldgexp_canada} to target exposure if different to run.  Change ogr2ogr path, location paths, db information if needed.
 
+REM Geopackage Shapefile export
+FOR %%x IN (nhsl_physical_exposure_all_indicators_ste) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_physical_exposure.%%x
 
 REM Geopackage Shapefile export
 FOR %%x IN (nhsl_physical_exposure_all_indicators_b) DO "C:\OSGeo4W64\bin\ogr2ogr.exe" -f "gpkg" D:\workspace\data\view_outputs\%%x.gpkg PG:"host=localhost user=postgres dbname=oq_sandbox_new_exposure password=password" results_nhsl_physical_exposure.%%x
