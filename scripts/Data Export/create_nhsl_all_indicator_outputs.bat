5d244f15ffaf1404f0234fc516ef238596cd4971
William Chow <59062243+wkhchow@users.noreply.github.com>
2020-11-27 16:50:28 -0800

    misc updates

--- scripts/Data Export/create_nhsl_all_indicator_outputs.bat
+++ scripts/Data Export/create_nhsl_all_indicator_outputs.bat
@@ -1,10 +1,10 @@
 REM Export views to geojson, and geopackage files. Replace exposure name {bldgexp_canada} to target exposure if different to run.  Change ogr2ogr path, location paths, db information if needed.
 
 REM Geopackage Shapefile export
-REM FOR %%x IN (nhsl_physical_exposure_all_indicators_b) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\merged_indicators\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" results_nhsl_physical_exposure.%%x
+FOR %%x IN (nhsl_physical_exposure_all_indicators_b) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\merged_indicators\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" results_nhsl_physical_exposure.%%x
 
 REM Geopackage Shapefile export
-FOR %%x IN (nhsl_physical_exposure_all_indicators_s) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\merged_indicators\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" results_nhsl_physical_exposure.%%x
+REM FOR %%x IN (nhsl_physical_exposure_all_indicators_s) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\merged_indicators\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" results_nhsl_physical_exposure.%%x
 
 REM Geopackage Shapefile export
 REM FOR %%x IN (nhsl_risk_dynamics_all_indicators) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\merged_indicators\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" results_nhsl_risk_dynamics.%%x
