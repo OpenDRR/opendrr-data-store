07e95aa0b3c5ae2e3b8846309ccf5bb493cd52f1
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-01-15 14:51:29 -0800

    Update create_psra_outputs_by_er_bc.bat

--- scripts/Data Export/create_psra_outputs_by_er_bc.bat
+++ scripts/Data Export/create_psra_outputs_by_er_bc.bat
@@ -8,9 +8,16 @@ FOR %%x IN (psra_bc_all_indicators_b,psra_bc_all_indicators_s) DO (FOR %%y IN (5
 FOR %%x IN (psra_bc_all_indicators_b, ^
 psra_bc_all_indicators_s, ^
 psra_bc_pml_s, ^
-psra_bc5910_src_loss, ^
-psra_bc5920_src_loss, ^
-psra_bc5930_src_loss, ^
-psra_bc5940_80_src_loss) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\seismic_risk\%%x_.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" results_psra_bc.%%x
+psra_bc_src_loss, ^
+psra_bc_hcurves_pga, ^
+psra_bc_hcurves_sa0p1, ^
+psra_bc_hcurves_sa0p2, ^
+psra_bc_hcurves_sa0p3, ^
+psra_bc_hcurves_sa0p5, ^
+psra_bc_hcurves_sa0p6, ^
+psra_bc_hcurves_sa1p0, ^
+psra_bc_hcurves_sa2p0, ^
+psra_bc_hmaps, ^
+psra_bc_uhs) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\seismic_risk\%%x_.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" results_psra_bc.%%x
 
 PAUSE
