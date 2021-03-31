9aa0423e3238dcbad33b7e89372dd7e0ec826582
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-03-31 16:43:04 -0700

    indicator revision/updates based on risk profile indicators

---
+++ scripts/Data Export/create_psra_outputs_national_sql_select_name.bat
@@ -0,0 +1,13 @@
+REM Export views from postgis db. Change ogr2ogr path, location paths, db information if needed.
+
+
+REM Geopackage national PSRA
+FOR %%x IN (psra_all_indicators_b, ^
+psra_all_indicators_s, ^
+psra_pml_s, ^
+psra_src_loss, ^
+psra_uhs) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\seismic_risk\layer_name\National\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" -sql "SELECT * FROM results_psra_national.%%x" -nln %%x
+
+
+
+
