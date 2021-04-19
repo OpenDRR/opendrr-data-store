8cbb7d4d9da2cf5aa8feb1788b872073bd2ab5fe
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-04-19 10:16:54 -0700

    Delete test.bat

--- scripts/Data Export/test.bat
+++
@@ -1,7 +0,0 @@
-ECHO %TIME%
-REM earthquake scenarios
-CD /D "D:\Workspace\data\view_outputs\all_indicators\test\" && FOR %%i IN (*.*) DO 7z.exe a "%%~ni.zip" "%%i"
-
-REM delete geopackages in folder
-DEL *.gpkg
-PAUSE
