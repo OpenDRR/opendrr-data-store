6ab27746ff8fabeab968eaa2d5d8a8d0bbe8cdde
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-04-19 10:19:52 -0700

    added new dsra scenarios

--- scripts/Data Export/3.create_dsra_outputs_combined.bat
+++ scripts/Data Export/3.create_dsra_outputs_combined.bat
@@ -15,7 +15,6 @@ dsra_idm6p0_sidney, ^
 dsra_idm6p2_sidney, ^
 dsra_idm6p4_sidney, ^
 dsra_idm6p6_sidney, ^
-dsra_idm6p8_jdfpathways, ^
 dsra_idm6p8_sidney, ^
 dsra_idm7p0_sidney, ^
 dsra_idm7p1_sidney, ^
@@ -23,7 +22,9 @@ dsra_idm7p1_sidneytest, ^
 dsra_scm6p5_ottawa, ^
 dsra_scm7p0_montrealnw, ^
 dsra_scm7p0_montrealnwcanshm5, ^
-dsra_sim9p0_cszlockedtrans) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\earthquake_scenarios\%%x_all_indicators_b.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" -sql "SELECT * FROM results_%%x.%%x_all_indicators_b" -nln %%x_all_indicators_b
+dsra_sim9p0_cszlockedtrans, ^
+dsra_acm7p3_georgiastraitnew, ^
+dsra_sim9p0_cascadiainterfacebestfault) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\earthquake_scenarios\%%x_all_indicators_b.gpkg PG:"host=localhost user=postgres dbname=opendrr password=admin" -sql "SELECT * FROM results_%%x.%%x_all_indicators_b" -nln %%x_all_indicators_b
 
 
 REM Geopackage dsra, _all_indicators_s
@@ -40,7 +41,6 @@ dsra_idm6p0_sidney, ^
 dsra_idm6p2_sidney, ^
 dsra_idm6p4_sidney, ^
 dsra_idm6p6_sidney, ^
-dsra_idm6p8_jdfpathways, ^
 dsra_idm6p8_sidney, ^
 dsra_idm7p0_sidney, ^
 dsra_idm7p1_sidney, ^
@@ -48,7 +48,9 @@ dsra_idm7p1_sidneytest, ^
 dsra_scm6p5_ottawa, ^
 dsra_scm7p0_montrealnw, ^
 dsra_scm7p0_montrealnwcanshm5, ^
-dsra_sim9p0_cszlockedtrans) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\earthquake_scenarios\%%x_all_indicators_s.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" -sql "SELECT * FROM results_%%x.%%x_all_indicators_s" -nln %%x_all_indicators_s
+dsra_sim9p0_cszlockedtrans, ^
+dsra_acm7p3_georgiastraitnew, ^
+dsra_sim9p0_cascadiainterfacebestfault) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\earthquake_scenarios\%%x_all_indicators_s.gpkg PG:"host=localhost user=postgres dbname=opendrr password=admin" -sql "SELECT * FROM results_%%x.%%x_all_indicators_s" -nln %%x_all_indicators_s
 
 
 REM Geopackage dsra, _shakemap
@@ -65,7 +67,6 @@ dsra_idm6p0_sidney, ^
 dsra_idm6p2_sidney, ^
 dsra_idm6p4_sidney, ^
 dsra_idm6p6_sidney, ^
-dsra_idm6p8_jdfpathways, ^
 dsra_idm6p8_sidney, ^
 dsra_idm7p0_sidney, ^
 dsra_idm7p1_sidney, ^
@@ -73,7 +74,9 @@ dsra_idm7p1_sidneytest, ^
 dsra_scm6p5_ottawa, ^
 dsra_scm7p0_montrealnw, ^
 dsra_scm7p0_montrealnwcanshm5, ^
-dsra_sim9p0_cszlockedtrans) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\earthquake_scenarios\%%x_shakemap.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" -sql "SELECT * FROM results_%%x.%%x_shakemap" -nln %%x_shakemap
+dsra_sim9p0_cszlockedtrans, ^
+dsra_acm7p3_georgiastraitnew, ^
+dsra_sim9p0_cascadiainterfacebestfault) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\earthquake_scenarios\%%x_shakemap.gpkg PG:"host=localhost user=postgres dbname=opendrr password=admin" -sql "SELECT * FROM results_%%x.%%x_shakemap" -nln %%x_shakemap
 
 
 REM Export DSRA master all scenarios views from postgis db into Geopackage. Change ogr2ogr path, location paths, db information if needed.
@@ -87,7 +90,11 @@ dsra_all_scenarios_dauid, ^
 dsra_all_scenarios_eruid, ^
 dsra_all_scenarios_fsauid, ^
 dsra_all_scenarios_pruid, ^
-dsra_all_scenarios_sauid) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\earthquake_scenarios\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr_allscenarios password=admin" -sql "SELECT * FROM dsra.%%x" -nln %%x
+dsra_all_scenarios_sauid
+) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\earthquake_scenarios\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr password=admin" -sql "SELECT * FROM dsra.%%x" -nln %%x
+
+REM Geopackage dsra, shakemap scenario extents
+FOR %%x IN (shakemap_scenario_extents) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\earthquake_scenarios\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr password=admin" -sql "SELECT * FROM gmf.%%x" -nln %%x
 
 ECHO %TIME% 
 PAUSE
