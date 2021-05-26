b64548837879f4db93b8f33fefd4ff36c5c13e8b
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-05-26 14:01:01 -0700

    Create Export_table_vs_30_CAN_site_model_xref.sql

---
+++ scripts/Source Tables/DSRA/building level up/v1.1/Export_table_vs_30_CAN_site_model_xref.sql
@@ -0,0 +1,3 @@
+-- script to export vs30_can_site_model_xref
+COPY vs30.vs30_can_site_model_xref TO 'D:\Workspace\GitHub\openquake-inputs\earthquake\sites\regions\vs30_CAN_site_model_xref.csv' DELIMITER ',' CSV HEADER;
+
