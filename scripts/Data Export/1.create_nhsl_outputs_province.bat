6cc0605f79a233d90fe958875750f5ab63c450fd
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-07-14 15:00:57 -0700

    Update 1.create_nhsl_outputs_province.bat

--- scripts/Data Export/1.create_nhsl_outputs_province.bat
+++ scripts/Data Export/1.create_nhsl_outputs_province.bat
@@ -79,21 +79,5 @@ nhsl_hazard_threat_all_indicators_s_qc, ^
 nhsl_hazard_threat_all_indicators_s_sk, ^
 nhsl_hazard_threat_all_indicators_s_yt) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\multi_hazard_threat\province\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr password=admin" -sql "SELECT * FROM results_nhsl_hazard_threat.%%x" -nln %%x
 
-REM Geopackage multi hazard threat
-FOR %%x IN (nhsl_hazard_threat_all_indicators_b, ^
-nhsl_hazard_threat_all_indicators_b_ab, ^
-nhsl_hazard_threat_all_indicators_b_bc, ^
-nhsl_hazard_threat_all_indicators_b_mb, ^
-nhsl_hazard_threat_all_indicators_b_nb, ^
-nhsl_hazard_threat_all_indicators_b_nl, ^
-nhsl_hazard_threat_all_indicators_b_ns, ^
-nhsl_hazard_threat_all_indicators_b_nt, ^
-nhsl_hazard_threat_all_indicators_b_nu, ^
-nhsl_hazard_threat_all_indicators_b_on, ^
-nhsl_hazard_threat_all_indicators_b_pe, ^
-nhsl_hazard_threat_all_indicators_b_qc, ^
-nhsl_hazard_threat_all_indicators_b_sk, ^
-nhsl_hazard_threat_all_indicators_b_yt) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\multi_hazard_threat\province\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr password=admin" -sql "SELECT * FROM results_nhsl_hazard_threat.%%x" -nln %%x
-
 ECHO %TIME% 
 PAUSE
