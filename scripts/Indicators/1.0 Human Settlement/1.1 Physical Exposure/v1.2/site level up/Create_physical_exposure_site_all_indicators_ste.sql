5e990aa45bf1768a0a9d213c4a6c7b0875983cbc
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-08-13 14:24:02 -0700

    update sf/mf indicators

--- scripts/Indicators/1.0 Human Settlement/1.1 Physical Exposure/v1.2/site level up/Create_physical_exposure_site_all_indicators_ste.sql
+++ scripts/Indicators/1.0 Human Settlement/1.1 Physical Exposure/v1.2/site level up/Create_physical_exposure_site_all_indicators_ste.sql
@@ -32,6 +32,22 @@ a.bldepoch AS "E_BldgEpoch",
 a.ssc_zone AS "SSC_Zone",
 a.eqdeslev AS "E_BldgDesLev",
 
+-- single family household, res units for RES1, RES2 = 1
+CAST(CAST(ROUND(CAST((CASE 
+					  WHEN a.occclass1 = 'RES1' THEN a.number * 1
+					  WHEN a.occclass1 = 'RES2' THEN a.number * 1 
+					  ELSE 0 END) AS NUMERIC),6) AS FLOAT) AS NUMERIC) AS "E_SFHshld",
+
+-- multi family household, res units for RES3A = 2, RES3B = 4, RES3C = 9, RES3D = 17, RES3E = 32, RES3F = 110
+CAST(CAST(ROUND(CAST((CASE 
+					  WHEN a.occclass1 = 'RES3A' THEN a.number * 2
+					  WHEN a.occclass1 = 'RES3B' THEN a.number * 4
+					  WHEN a.occclass1 = 'RES3C' THEN a.number * 9
+					  WHEN a.occclass1 = 'RES3D' THEN a.number * 17
+					  WHEN a.occclass1 = 'RES3E' THEN a.number * 32
+					  WHEN a.occclass1 = 'RES3F' THEN a.number * 110
+					  ELSE 0 END) AS NUMERIC),6) AS FLOAT) AS NUMERIC) AS "E_MFHshld",
+
 -- 1.0 Human Settlement
 -- 1.1 Physical Exposure
 -- 1.1.2 People
