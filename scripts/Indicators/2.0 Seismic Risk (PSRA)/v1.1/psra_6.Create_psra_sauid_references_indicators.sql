127d3326460eec0de964ea5ed003db76ad0ba5da
William Chow <59062243+wkhchow@users.noreply.github.com>
2022-01-10 15:06:10 -0800

    Update psra_6.Create_psra_sauid_references_indicators.sql

--- scripts/Indicators/2.0 Seismic Risk (PSRA)/v1.1/psra_6.Create_psra_sauid_references_indicators.sql
+++ scripts/Indicators/2.0 Seismic Risk (PSRA)/v1.1/psra_6.Create_psra_sauid_references_indicators.sql
@@ -1,21 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS results_psra_{prov};
 
-/*
--- src loss values
-DROP VIEW IF EXISTS results_psra_{prov}.psra_{prov}_src_loss CASCADE;
-CREATE VIEW results_psra_{prov}.psra_{prov}_src_loss AS
-
-SELECT 
-source AS "src_zone",
-SUM(loss_value_b0)/(SELECT SUM(loss_value_b0) FROM psra_{prov}.psra_{prov}_src_loss ) AS "src_value_b0",
-SUM(loss_value_r1)/(SELECT SUM(loss_value_r1) FROM psra_{prov}.psra_{prov}_src_loss ) AS "src_value_r1"
-FROM psra_{prov}.psra_{prov}_src_loss
-GROUP BY source
-ORDER BY source ASC;
-*/
-
-
-
 
 -- src loss values
 DROP TABLE IF EXISTS results_psra_{prov}.psra_{prov}_src_loss_temp CASCADE;
@@ -43,12 +27,12 @@ ORDER BY a.source ASC;
 DROP TABLE IF EXISTS results_psra_{prov}.psra_{prov}_src_loss_tbl CASCADE;
 CREATE TABLE results_psra_{prov}.psra_{prov}_src_loss_tbl AS
 SELECT
-src_zone AS "eS_Zone",
-src_type AS "eS_Type",
-loss_type AS "eS_LossType",
-region AS "e_Aggregation",
-CAST(CAST(ROUND(CAST(SUM(src_value_b0) AS NUMERIC),6) AS FLOAT) AS NUMERIC) AS "eS_Value_b0",
-CAST(CAST(ROUND(CAST(SUM(src_value_r1) AS NUMERIC),6) AS FLOAT) AS NUMERIC) AS "eS_value_r1"
+src_zone,
+src_type,
+loss_type,
+region,
+CAST(CAST(ROUND(CAST(SUM(src_value_b0) AS NUMERIC),6) AS FLOAT) AS NUMERIC) AS "src_value_b0",
+CAST(CAST(ROUND(CAST(SUM(src_value_r1) AS NUMERIC),6) AS FLOAT) AS NUMERIC) AS "src_value_r1"
 
 FROM results_psra_{prov}.psra_{prov}_src_loss_temp
 GROUP BY src_zone,src_type,loss_type,region;
@@ -62,16 +46,10 @@ DROP TABLE IF EXISTS results_psra_{prov}.psra_{prov}_src_loss_temp,psra_{prov}.p
 
 
 
-
 DROP VIEW IF EXISTS results_psra_{prov}.psra_{prov}_hcurves_pga,results_psra_{prov}.psra_{prov}_hcurves_sa0p1,results_psra_{prov}.psra_{prov}_hcurves_sa0p2,results_psra_{prov}.psra_{prov}_hcurves_sa0p3,
 results_psra_{prov}.psra_{prov}_hcurves_sa0p5,results_psra_{prov}.psra_{prov}_hcurves_sa0p6,results_psra_{prov}.psra_{prov}_hcurves_sa1p0,results_psra_{prov}.psra_{prov}_hcurves_sa2p0,results_psra_{prov}.psra_{prov}_hmaps,
 results_psra_{prov}.psra_{prov}_uhs,results_psra_{prov}.psra_{prov}_hmaps_xref CASCADE;
 
---CREATE VIEW results_psra_{prov}.psra_{prov}_agg_curves_stats AS SELECT * FROM psra_{prov}.psra_{prov}_agg_curves_stats;
-
---CREATE VIEW results_psra_{prov}.psra_{prov}_agg_curves_q05 AS SELECT * FROM psra_{prov}.psra_{prov}_agg_curves_q05;
-
---CREATE VIEW results_psra_{prov}.psra_{prov}_agg_curves_q95 AS SELECT * FROM psra_{prov}.psra_{prov}_agg_curves_q95;
 
 CREATE VIEW results_psra_{prov}.psra_{prov}_hcurves_pga AS SELECT FROM psra_{prov}.psra_{prov}_hcurves_pga;
 
