f74fc2fa6bc347563a524c28dfa98da975dcfbe8
William Chow <59062243+wkhchow@users.noreply.github.com>
2022-05-10 09:37:17 -0700

    hexbin to hexgrid

--- scripts/Data Export/1a.create_nhsl_hexgrid_outputs.bat
+++ scripts/Data Export/1a.create_nhsl_hexgrid_outputs.bat
@@ -2,16 +2,16 @@ REM Export NHSL hexgrid views from postgis db into GeoPackages. Change ogr2ogr p
 
 ECHO %TIME% 
 REM Geopackage physical exposure
-FOR %%x IN (nhsl_physical_exposure_indicators_hexbin_1km, ^
-nhsl_physical_exposure_indicators_hexbin_1km_uc, ^
-nhsl_physical_exposure_indicators_hexbin_5km, ^
-nhsl_physical_exposure_indicators_hexbin_5km_uc, ^
-nhsl_physical_exposure_indicators_hexbin_10km, ^
-nhsl_physical_exposure_indicators_hexbin_10km_uc, ^
-nhsl_physical_exposure_indicators_hexbin_25km, ^
-nhsl_physical_exposure_indicators_hexbin_25km_uc, ^
-nhsl_physical_exposure_indicators_hexbin_50km_uc, ^
-nhsl_physical_exposure_indicators_hexbin_100km_uc) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\hexgrid\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr password=admin" -sql "SELECT * FROM results_nhsl_physical_exposure.%%x" -nln %%x
+FOR %%x IN (nhsl_physical_exposure_indicators_hexgrid_1km, ^
+nhsl_physical_exposure_indicators_hexgrid_1km_uc, ^
+nhsl_physical_exposure_indicators_hexgrid_5km, ^
+nhsl_physical_exposure_indicators_hexgrid_5km_uc, ^
+nhsl_physical_exposure_indicators_hexgrid_10km, ^
+nhsl_physical_exposure_indicators_hexgrid_10km_uc, ^
+nhsl_physical_exposure_indicators_hexgrid_25km, ^
+nhsl_physical_exposure_indicators_hexgrid_25km_uc, ^
+nhsl_physical_exposure_indicators_hexgrid_50km_uc, ^
+nhsl_physical_exposure_indicators_hexgrid_100km_uc) DO ogr2ogr -f "gpkg" D:\Workspace\data\view_outputs\all_indicators\hexgrid\%%x.gpkg PG:"host=localhost user=postgres dbname=opendrr password=admin" -sql "SELECT * FROM results_nhsl_physical_exposure.%%x" -nln %%x
 
 REM Geopackage multi hazard threat
 FOR %%x IN (nhsl_hazard_threat_prioritization_hexgrid_1km, ^
